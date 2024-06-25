7caab5acd032ed67f89fc9ce537510da4299a5fa
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-06-25 15:55:08 +0700

    xoa code ko can thiet

--- app/src/main/java/com/example/testzalopaykotlin/Helper/HMac/HexStringUtil.kt
+++ app/src/main/java/com/example/testzalopaykotlin/Helper/HMac/HexStringUtil.kt
@@ -1,5 +1,4 @@
 package com.example.testzalopaykotlin.Helper.HMac
-import java.util.*
 
 object HexStringUtil {
     private val HEX_CHAR_TABLE = charArrayOf(
@@ -9,12 +8,6 @@ object HexStringUtil {
         'c', 'd', 'e', 'f'
     )
 
-    /**
-     * Convert a byte array to a hexadecimal string
-     *
-     * @param raw A raw byte array
-     * @return Hexadecimal string
-     */
     fun byteArrayToHexString(raw: ByteArray): String {
         val hex = CharArray(2 * raw.size)
         var index = 0
@@ -27,39 +20,4 @@ object HexStringUtil {
         return String(hex)
     }
 
-    /**
-     * Convert a hexadecimal string to a byte array
-     *
-     * @param hex A hexadecimal string
-     * @return The byte array
-     */
-    fun hexStringToByteArray(hex: String): ByteArray {
-        val hexStandard = hex.lowercase(Locale.ENGLISH)
-        val sz = hexStandard.length / 2
-        val bytesResult = ByteArray(sz)
-
-        var idx = 0
-        var i = 0
-        while (i < sz) {
-            bytesResult[i] = (hexStandard[idx]).code.toByte()
-            idx++
-            var tmp = (hexStandard[idx]).code.toByte()
-            idx++
-
-            if (bytesResult[i] > HEX_CHAR_TABLE[9].code.toByte()) {
-                bytesResult[i] = (bytesResult[i] - ('a'.code.toByte() - 10)).toByte()
-            } else {
-                bytesResult[i] = (bytesResult[i] - '0'.code.toByte()).toByte()
-            }
-            if (tmp > HEX_CHAR_TABLE[9].code.toByte()) {
-                tmp = (tmp - ('a'.code.toByte() - 10)).toByte()
-            } else {
-                tmp = (tmp - '0'.code.toByte()).toByte()
-            }
-
-            bytesResult[i] = (bytesResult[i] * 16 + tmp).toByte()
-            i++
-        }
-        return bytesResult
-    }
 }
